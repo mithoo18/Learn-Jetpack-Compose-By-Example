9b35074dbdcb98d12e2eb5ac1eb1f575fddd0f59
mithoo18 <devanshu18g@gmail.com>
2022-04-02 14:51:48 +0530

    Starting 1

--- app/src/main/java/com/example/learn_jetpack_compose_by_example/MainActivity.kt
+++ app/src/main/java/com/example/learn_jetpack_compose_by_example/MainActivity.kt
@@ -14,25 +14,8 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            LearnJetpackComposeByExampleTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    LearnJetpackComposeByExampleTheme {
-        Greeting("Android")
-    }
-}
