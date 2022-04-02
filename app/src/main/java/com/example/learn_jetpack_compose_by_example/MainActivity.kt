c3e34e0c050bd54a008a50e4b32b54666c0b0a56
mithoo18 <devanshu18g@gmail.com>
2022-04-02 15:20:17 +0530

    Starting 2

--- app/src/main/java/com/example/learn_jetpack_compose_by_example/MainActivity.kt
+++ app/src/main/java/com/example/learn_jetpack_compose_by_example/MainActivity.kt
@@ -3,19 +3,100 @@ package com.example.learn_jetpack_compose_by_example
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.learn_jetpack_compose_by_example.ui.theme.LearnJetpackComposeByExampleTheme
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+        //Text("Hello Kotlin", fontSize = 30.sp)
+        //Disp1()
+        //Disp2("Devanshu")
+        //Disp3()
+        //Disp4()
+        //Disp5()
+        //Disp6()
+        //Disp7()
+        }
+    }
 
+    //Ya Simple Hai
+    @Composable
+    fun Disp1() {
+        Text("Hello Kotlin", fontSize = 30.sp)
+    }
+
+    //Parameter Passsing
+    @Composable
+    fun Disp2(name : String){
+        Text("Hello Kt $name")
+    }
+
+    //Multiple Text But Problem is overlap
+    @Composable
+    fun Disp3()
+    {
+        Text("Hello 1", fontSize = 30.sp)
+        Text("Hello 2", fontSize = 30.sp)
+        Text("Hello 3", fontSize = 30.sp)
+        Text("Hello 4", fontSize = 30.sp)
+        Text("Hello 5", fontSize = 30.sp)
+    }
+
+    //1 ka nicha 1
+    @Composable
+    fun Disp4(){
+        Column {
+            Text("Hello 1", fontSize = 30.sp)
+            Text("Hello 2", fontSize = 30.sp)
+            Text("Hello 3", fontSize = 30.sp)
+            Text("Hello 4", fontSize = 30.sp)
+        }
+    }
+
+    //Loop
+    //Multiple Text But Problem is overlap
+    @Composable
+    fun Disp5(){
+        val names = listOf<String>("Rahul1","Rahul2","Rahul3","Rahul4")
+        for(name in names){
+            Text("Hello $name")
+        }
+
+    }
+
+    //Loop
+    //1 ka nicha 1
+    @Composable
+    fun Disp6(){
+        Column{
+            val names  = listOf<String>("Rahul1","Rahul2","Rahul3","Rahul4")
+            for(name in names){
+                Text("Hello $name")
             }
         }
     }
+
+    @Composable
+    fun Disp7(){
+        Text(
+            "Hello Kotlin",
+            fontSize = 30.sp,
+            color = Color.Blue,
+            fontStyle = FontStyle.Italic,
+            fontWeight = FontWeight.ExtraBold
+        )
+
+    }
+
+
+
+
+}
 
